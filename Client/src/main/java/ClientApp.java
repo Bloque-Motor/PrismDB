807ba24bc310e89b4622dab558b8af08b1b4647e
Ignacio de las Alas-Pumariño Martínez <i.dmartinez@alumnos.upm.es>
2018-12-11 20:48:19 +0100

    Started implementation of search method

--- Client/src/main/java/ClientApp.java
+++ Client/src/main/java/ClientApp.java
@@ -1,35 +1,74 @@
-import GUI.ClientGUI;
-import interfaces.AddUser;
+import interfaces.People;
+import interfaces.Prism;
 
-import javax.swing.*;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
 
 public class ClientApp {
 
     public static void main(String[] args) {
 
-        SwingUtilities.invokeLater(new Runnable() {
-            public void run() {
-                ClientGUI gui = new ClientGUI();
-                gui.setTitle("RMI Client");
-                gui.setSize(800,600);
-                gui.setContentPane(new ClientGUI().panelMain);
-                gui.setLocationRelativeTo(null);
-                gui.setDefaultCloseOperation(ClientGUI.EXIT_ON_CLOSE);
-                gui.setVisible(true);
-            }
-        });
+//        SwingUtilities.invokeLater(new Runnable() {
+//            public void run() {
+//                ClientGUI gui = new ClientGUI();
+//                gui.setTitle("RMI Client");
+//                gui.setContentPane(new ClientGUI().panelMain);
+//                gui.setLocationRelativeTo(null);
+//                gui.setDefaultCloseOperation(ClientGUI.EXIT_ON_CLOSE);
+//                gui.setVisible(true);
+//            }
+//        });
+    }
 
+    public static People search() {
+        //String name, String surname, String dni, String phone, String email
+        System.out.println("Introduzca un nombre: ");
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.nextLine();
+        System.out.println("Introduzca un apellido: ");
+        String surname = scanner.nextLine();
+        System.out.println("Introduzca un dni: ");
+        String dni = scanner.nextLine();
+        System.out.println("Introduzca un telefono: ");
+        String phone = scanner.nextLine();
+        System.out.println("Introduzca un email: ");
+        String email = scanner.nextLine();
+
+        Map<String, String> searchParam = new HashMap<>();
+
+        if (!name.equals("")) searchParam.put("name", name);
+        if (!surname.equals("")) searchParam.put("surname", surname);
+        if (!dni.equals("")) searchParam.put("dni", dni);
+        if (!phone.equals("")) searchParam.put("phone", phone);
+        if (!email.equals("")) searchParam.put("email", email);
+
+        Registry registry = null;
+        People res = null;
+        try {
+            registry = LocateRegistry.getRegistry();
+            Prism stub = (Prism) registry.lookup("Prism");
+            res = stub.searchUser(name);
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+
+        return res;
     }
 
     public void addUser (String name, String surname, String dni, String phone, String email){
         Registry registry = null;
         try {
             registry = LocateRegistry.getRegistry();
-            AddUser stub = (AddUser) registry.lookup("AddUser");
+            Prism stub = (Prism) registry.lookup("Prism");
+            stub.addUser(name, surname, dni, phone, email);
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    public abstract class Person implements People {
     }
 }
