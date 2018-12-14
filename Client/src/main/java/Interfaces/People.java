9eae1b2bd1f8314b3612bcc68295c54323250978
Ignacio de las Alas-Pumariño Martínez <i.dmartinez@alumnos.upm.es>
2018-12-14 19:23:49 +0100

    Cloned inerfaces from server to client

--- Client/src/main/java/Interfaces/People.java
+++ Client/src/main/java/Interfaces/People.java
@@ -1,39 +1,48 @@
 package Interfaces;
 
-public interface People extends java.rmi.Remote {
+import java.rmi.Remote;
+import java.rmi.RemoteException;
 
-    String getName() throws java.rmi.RemoteException;
+public interface People extends Remote {
 
-    String getSurname()throws java.rmi.RemoteException;
+    String getName() throws RemoteException;
 
-    String getDni() throws java.rmi.RemoteException;
+    String getSurname() throws RemoteException;
 
-    String getTelephone() throws java.rmi.RemoteException;
+    String getDni() throws RemoteException;
 
-    String getEmail() throws java.rmi.RemoteException;
+    String getTelephone() throws RemoteException;
 
-    void setName(String name) throws java.rmi.RemoteException;
+    String getEmail() throws RemoteException;
 
-    void setSurname(String surname) throws java.rmi.RemoteException;
+    void setName(String name) throws RemoteException;
 
-    void setDni(String dni) throws java.rmi.RemoteException;
+    void setSurname(String surname) throws RemoteException;
 
-    void setTelephone(String telephone) throws java.rmi.RemoteException;
+    void setDni(String dni) throws RemoteException;
 
-    void setEmail(String email) throws java.rmi.RemoteException;
+    void setTelephone(String telephone) throws RemoteException;
+
+    void setEmail(String email) throws RemoteException;
+
+    // For Name and Surname
+    // Admits compound names and surnames, check them together
+    static boolean isName(String name){
+        return name.length() >= 3 && name.length() <= 50;
+    }
 
     static boolean isDni(String dni) {
         String pattern = "[0-9]{7,8}(\\s*-?\\s*)?[A-Za-z]";
-        return dni.matches(pattern);
+        return dni.length() >= 8 && dni.length() <= 11 && dni.matches(pattern);
     }
 
     static boolean isTelephone(String telephone) {
         String pattern = "(\\+(0[1-9][0-9]?|[1-9][0-9]{0,2}))?\\s*[1-9]{3}\\s*[0-9]{3}\\s*[0-9]{3}";
-        return telephone.matches(pattern);
+        return telephone.length() >= 9 && telephone.length() <= 15 && telephone.matches(pattern);
     }
 
     static boolean isEmail(String email) {
         String pattern = "(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])";
-        return email.matches(pattern);
+        return email.length() >= 8 && email.length() <= 100 && email.matches(pattern);
     }
 }
--- Client/src/main/java/Interfaces/Prism.java
+++ Client/src/main/java/Interfaces/Prism.java
@@ -1,14 +1,15 @@
 package Interfaces;
-
+import java.rmi.Remote;
+import java.rmi.RemoteException;
 import java.util.Map;
 
-public interface Prism extends java.rmi.Remote {
+public interface Prism extends Remote {
 
-    void addUser(String name, String surname, String dni, String email, String phone) throws java.rmi.RemoteException;
+    boolean addUser(String name, String surname, String dni, String email, String phone) throws RemoteException;
 
-    People searchUser(Map params) throws java.rmi.RemoteException;
+    People searchUser(Map params) throws RemoteException;
 
-    void deleteUser(String dni) throws java.rmi.RemoteException;
+    boolean deleteUser(People user) throws RemoteException;
 
-    void updateUser(String oldDni, String name, String surname, String dni, String email, String phone) throws java.rmi.RemoteException;
+    boolean updateUser(String oldDni, People user) throws RemoteException;
 }
