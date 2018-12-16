19c1d88c07ba07c74de54ea41e18f88795db46ae
Pablo Beltrán de Casso <beltrancasso@hotmail.com>
2018-12-16 17:02:50 +0100

    Added keyType on people and changed Prism People to Map

--- Backend/src/main/Interfaces/Prism.java
+++ Backend/src/main/Interfaces/Prism.java
@@ -1,15 +1,16 @@
 package Interfaces;
 import java.rmi.Remote;
 import java.rmi.RemoteException;
+import java.util.ArrayList;
 import java.util.Map;
 
 public interface Prism extends Remote {
 
     boolean addUser(String dni, String name, String surname, String email, String phone) throws RemoteException;
 
-    People searchUser(Map params) throws RemoteException;
+    ArrayList<Map<String, String>> searchUser(Map params) throws RemoteException;
 
-    boolean deleteUser(People user) throws RemoteException;
+    boolean deleteUser(Map<String, String> user) throws RemoteException;
 
-    boolean updateUser(String oldDni, People user) throws RemoteException;
+    boolean updateUser(String oldDni, Map<String, String> user) throws RemoteException;
 }
